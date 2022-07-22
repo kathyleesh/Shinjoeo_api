6a4b583a03c769b7345c39fa4c1275fe71c98c96
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-07-23 07:19:52 +0900

    change kakao

--- Shinjoeo/shinjoeo/accounts/views.py
+++ Shinjoeo/shinjoeo/accounts/views.py
@@ -13,7 +13,10 @@ from django.contrib.auth.decorators import login_required
 @api_view(['GET'])
 @permission_classes([AllowAny, ])
 def kakaoGetLogin(request):
-    url = "http://shinjoeo.s3-website.ap-northeast-2.amazonaws.com/main"
+    CLIENT_ID = KAKAO_CONFIG['KAKAO_REST_API_KEY']
+    REDIRET_URL = KAKAO_CONFIG['KAKAO_REDIRECT_URI']
+    url = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={0}&redirect_uri={1}".format(
+        CLIENT_ID, REDIRET_URL)
     res = redirect(url)
     return res
 
