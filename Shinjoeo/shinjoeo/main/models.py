1182184abcecd8b78f53c9ae880e6401c2d3257e
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-07-22 21:23:11 +0900

    fix login, callback, permission

--- Shinjoeo/shinjoeo/main/models.py
+++ Shinjoeo/shinjoeo/main/models.py
@@ -9,7 +9,7 @@ class NewWord(models.Model):
     explain = models.TextField(blank=False)
     like_user_ids = models.ManyToManyField(User,related_name='likeword', blank=True)
     create_time = models.DateTimeField(auto_now_add=True) 
-    create_user_id = models.ForeignKey(User, to_field= 'username',related_name = "newword", on_delete=models.CASCADE,db_column="create_user_id")
+    create_user_id = models.ForeignKey(User,related_name = "newword", on_delete=models.CASCADE,db_column="create_user_id")
     likeCnt = models.IntegerField(default=0)
    
     def __str__(self):
